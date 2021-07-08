f678c54d0e7090e008d9bc88ba17c573b2d9bdae
GonzaloPerez1 <71379299+GonzaloPerez1@users.noreply.github.com>
2021-07-08 08:47:28 +0200

    Delete constants.py

--- constants.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-#Creamos los tableros de los jugadores
-tablero_juego = np.full((10,10), '~') #Tablero del jugador 1 donde estarán los barcos
-tablero_juego_maquina = np.full((10,10), '~') #Tablero de la máquina con los barcos
-tablero_guia = np.full((10,10), '~') #Tablero del jugador 1 donde estarán los barcos
-
-
